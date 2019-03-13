7a51ebee9f5913be1ce15f46f4d2d3c383aa421e
luolanmeet <mainem@163.com>
2019-03-13 18:30:05 +0800

    IGNORE

---
+++ linked-list/src/pers/SplitListToParts.java
@@ -0,0 +1,14 @@
+package pers;
+
+/**
+ * 725. 分隔链表
+ * https://leetcode-cn.com/problems/split-linked-list-in-parts/
+ * @author cck
+ */
+public class SplitListToParts {
+    
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        return null;
+    }
+
+}
