c219af4ee829f23539eeebe7fd36977781ad66b8
luolanmeet <mainem@163.com>
2019-03-13 20:32:29 +0800

    add lc 725

--- linked-list/src/pers/README.md
+++ linked-list/src/pers/README.md
@@ -20,3 +20,4 @@ ReverseList         | `LC简单题`[206. 反转链表](https://leetcode-cn.com/p
 OddEvenList         | `LC中等题`[328. 奇偶链表](https://leetcode-cn.com/problems/odd-even-linked-list/)
 Flatten             | `LC中等题`[430. 扁平化多级双向链表](https://leetcode-cn.com/problems/flatten-a-multilevel-doubly-linked-list/)
 AddTwoNumbers2      | `LC中等题`[445. 两数相加 II](https://leetcode-cn.com/problems/add-two-numbers-ii/)
+SplitListToParts    | `LC中等题`[725. 分隔链表](https://leetcode-cn.com/problems/split-linked-list-in-parts/)
--- linked-list/src/pers/SplitListToParts.java
+++ linked-list/src/pers/SplitListToParts.java
@@ -8,7 +8,71 @@ package pers;
 public class SplitListToParts {
     
     public ListNode[] splitListToParts(ListNode root, int k) {
-        return null;
+        
+        // 先统计链表长度，然后计算每个链表的长度
+        ListNode temp = root;
+        int len = 0;
+        while (temp != null) {
+            temp = temp.next;
+            len++;
+        }
+
+        int[] lens = new int[k]; 
+        for (int i = 0; i < len % k; i++) {
+            lens[i] = 1;
+        }
+        
+        ListNode[] resp = new ListNode[k];
+        int count = len / k;
+        for (int i = 0; i < k; i++) {
+            
+            resp[i] = root;
+            for (int j = 1; j < count + lens[i]; j++) {
+                root = root.next;
+            }
+            
+            if (root != null) {
+                temp = root.next;
+                root.next = null;
+                root = temp;
+            }
+        }
+        
+        return resp;
     }
 
+    public static void main(String[] args) {
+        
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        ListNode l7 = new ListNode(7);
+        ListNode l8 = new ListNode(8);
+        ListNode l9 = new ListNode(9);
+        ListNode l10 = new ListNode(10);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+        l6.next = l7;
+        l7.next = l8;
+        l8.next = l9;
+        l9.next = l10;
+
+        SplitListToParts obj = new SplitListToParts();
+        ListNode[] resp = obj.splitListToParts(l1, 3);
+        
+        for (ListNode head : resp) {
+            while (head != null) {
+                System.out.print(head.val + " ");
+                head = head.next;
+            }
+            System.out.println();
+        }
+    }
+    
 }
