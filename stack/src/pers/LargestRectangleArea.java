63dd14552661beda83c2d536e764c970fdb35c95
luolanmeet <mainem@163.com>
2019-03-19 19:11:54 +0800

    IGNORE

--- stack/src/pers/LargestRectangleArea.java
+++ stack/src/pers/LargestRectangleArea.java
@@ -28,6 +28,8 @@ public class LargestRectangleArea {
                 
                 // 认为当前index的高度是最小的高度，用当前高度去乘宽度
                 // 注意，这里的 i 是没变化的，i停留在当前遍历到最右边，用 i 去计算宽度
+                // 那些高度连续递增的那些长方形一直被压入栈
+                // 递增中断时，弹出，计算面积，然后重复这个过程，继续压栈
                 int index = stack.pop();
                 res = Math.max(res, h[index] * (stack.isEmpty() ? i : i - stack.peek() - 1));
             }
