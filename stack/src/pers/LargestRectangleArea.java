de55e2a1dd1c89ebd9bfb985395068a1d8212d83
luolanmeet <mainem@163.com>
2019-03-19 19:03:18 +0800

    add lc 84

---
+++ stack/src/pers/LargestRectangleArea.java
@@ -0,0 +1,45 @@
+package pers;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * 84. 柱状图中最大的矩形
+ * https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
+ * @author cck
+ */
+public class LargestRectangleArea {
+
+    public int largestRectangleArea(int[] heights) {
+        
+        // 单调递增栈
+        Stack<Integer> stack = new Stack<>();
+        int[] h = Arrays.copyOf(heights, heights.length + 1);
+        
+        int i = 0;
+        int res = 0;
+        
+        while (i < h.length) {
+            
+            if (stack.isEmpty() || h[i] > h[stack.peek()]) {
+                stack.push(i);
+                i++;
+            } else {
+                
+                // 认为当前index的高度是最小的高度，用当前高度去乘宽度
+                // 注意，这里的 i 是没变化的，i停留在当前遍历到最右边，用 i 去计算宽度
+                int index = stack.pop();
+                res = Math.max(res, h[index] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+            }
+        }
+        
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        
+        LargestRectangleArea obj = new LargestRectangleArea();
+        System.out.println(obj.largestRectangleArea(new int[]{2,1,5,6,2,3}));
+    }
+    
+}
--- stack/src/pers/README.md
+++ stack/src/pers/README.md
@@ -3,6 +3,7 @@
 
 类名|内容
 ---|---
+LargestRectangleArea  | `LC困难题`[84. 柱状图中最大的矩形](https://leetcode-cn.com/problems/largest-rectangle-in-histogram/)
 EvalRPN               | `LC中等题`[150. 逆波兰表达式求值](https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/)
 DecodeString          | `LC中等题`[394. 字符串解码](https://leetcode-cn.com/problems/decode-string/)
 NextGreaterElement    | `LC简单题`[496. 下一个更大元素 I](https://leetcode-cn.com/problems/next-greater-element-i/)
