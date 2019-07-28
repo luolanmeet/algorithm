32160e88a7c02228f554a55c3a6ded51f8295f23
luolanmeet <mainem@163.com>
2019-07-29 07:58:42 +0800

    69. x 的平方根

---
+++ other/src/pers/MySqrt.java
@@ -0,0 +1,56 @@
+package pers;
+
+public class MySqrt {
+
+    // 牛顿迭代法
+    public int mySqrt(int x) {
+
+        long l = x;
+        
+        while (l * l > x) {
+            l = (l + x / l) / 2;
+        }
+        
+        return (int) l;
+    }
+
+    // 用二分法
+    public int mySqrt2(int x) {
+
+        if (x <= 0) {
+            return 0;
+        }
+
+        int left = 1;
+        int right = x;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            int result = x / mid;
+
+            if (result == mid) {
+                return mid;
+            }
+
+            if (result < mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+    
+    public static void main(String[] args) {
+        
+        MySqrt obj = new MySqrt();
+        System.out.println(obj.mySqrt(3));
+        System.out.println(obj.mySqrt2(3));
+        
+        System.out.println(obj.mySqrt(14));
+        System.out.println(obj.mySqrt2(14));
+    }
+    
+}
--- other/src/pers/README.md
+++ other/src/pers/README.md
@@ -1,6 +1,7 @@
-# 常见的算法思路  
+# 常见的算法  
 
 算法|类名|lc链接
 ---|---|---
+**牛顿迭代法** | MySqrt | [69. x 的平方根](https://leetcode-cn.com/problems/sqrtx/)
 **区间加法** | CarPooling | [1094. 拼车](https://leetcode-cn.com/problems/car-pooling/)
-|          | CorpFlightBookings | [5118. 航班预订统计](https://leetcode-cn.com/problems/corporate-flight-bookings/)
+           | CorpFlightBookings | [5118. 航班预订统计](https://leetcode-cn.com/problems/corporate-flight-bookings/)
