00e6898c16a38c3671e4a1977e3aa8de1981e955
luolanmeet <mainem@163.com>
2019-08-05 18:35:47 +0800

    IGNORE

--- other/src/pers/README.md
+++ other/src/pers/README.md
@@ -4,5 +4,6 @@
 ---|---|---
 |**牛顿迭代法** | MySqrt | [69. x 的平方根](https://leetcode-cn.com/problems/sqrtx/)
 |**LRU缓存机制** | LRUCache | [146. LRU缓存机制](https://leetcode-cn.com/problems/lru-cache/)
+|**快照数组** | SnapshotArray | [1146. 快照数组](https://leetcode-cn.com/contest/weekly-contest-148/problems/snapshot-array/)
 |**区间加法** | CarPooling | [1094. 拼车](https://leetcode-cn.com/problems/car-pooling/)
 |          | CorpFlightBookings | [5118. 航班预订统计](https://leetcode-cn.com/problems/corporate-flight-bookings/)
---
+++ other/src/pers/SnapshotArray.java
@@ -0,0 +1,35 @@
+package pers;
+/**
+ * 1146. 快照数组
+ * https://leetcode-cn.com/contest/weekly-contest-148/problems/snapshot-array/
+ * @author cck
+ */
+public class SnapshotArray {
+    
+    private static class Value {
+        public int version;
+        public int value;
+        public Value(int version, int value) {
+            this.version = version;
+            this.value = value;
+        }
+    }
+    
+    public SnapshotArray(int length) {
+    
+    }
+    
+    public void set(int index, int val) {
+    
+    }
+    
+    public int snap() {
+        return 1;
+    }
+    
+    public int get(int index, int snap_id) {
+     
+        return 0;
+    }
+    
+}
