3a26701c5212c6a2f8e89bd6a999391c3a0c9a9d
luolanmeet <mainem@163.com>
2019-09-28 17:06:32 +0800

    IGNORE

---
+++ other/src/pers/WordDictionary.java
@@ -0,0 +1,24 @@
+package pers;
+
+/**
+ * 211. 添加与搜索单词 - 数据结构设计
+ * https://leetcode-cn.com/problems/add-and-search-word-data-structure-design/
+ */
+public class WordDictionary {
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+
+    }
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return false;
+    }
+
+}
