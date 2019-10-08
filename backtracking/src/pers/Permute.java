705657cfba247c9b73d35993cd2f9dc48b90c9f2
luolanmeet <mainem@163.com>
2019-10-08 09:51:12 +0800

    修改写法

--- backtracking/src/pers/Permute.java
+++ backtracking/src/pers/Permute.java
@@ -1,8 +1,8 @@
 package pers;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 46. 全排列
@@ -11,6 +11,43 @@ import java.util.List;
  */
 public class Permute {
     
+    public List<List<Integer>> permute(int[] nums) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(new Stack<Integer>(), res, used, nums);
+        
+        return res;
+    }
+    
+    private void backtrack(Stack<Integer> stack,
+                           List<List<Integer>> res,
+                           boolean[] used,
+                           int[] nums) {
+        
+        if (stack.size() == nums.length) {
+            res.add(new ArrayList<>(stack));
+            return ;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+        
+            if (used[i]) {
+                continue;
+            }
+        
+            used[i] = true;
+            stack.push(nums[i]);
+            
+            backtrack(stack, res, used, nums);
+    
+            used[i] = false;
+            stack.pop();
+        }
+    }
+    
+    /*
     public List<List<Integer>> permute(int[] nums) {
         
         List<List<Integer>> res = new ArrayList<>();
@@ -41,7 +78,7 @@ public class Permute {
             backtrack(n, numsList, res, first + 1);
             Collections.swap(numsList, first, i);
         }
-    }
+    }*/
     
     public static void main(String[] args) {
         
