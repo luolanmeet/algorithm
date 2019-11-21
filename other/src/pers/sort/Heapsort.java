61a45b1c5a061c44226d67f80dc0d74222b35dac
luolanmeet <mainem@163.com>
2019-11-21 23:28:50 +0800

    简化写法

--- other/src/pers/sort/Heapsort.java
+++ other/src/pers/sort/Heapsort.java
@@ -35,29 +35,27 @@ public class Heapsort {
 
     private static void adjustHeap(int[] array, int i, int len) {
 
-        // 父节点要大于子节点 array[i] > array[i*2] array[i*2+1]
-        int k = i;
-        int tmp = array[i];
+        // 父节点要大于子节点 array[i] > array[i*2+1] array[i*2+2]
 
         // while(有子节点)
-        while (k * 2 + 1 < len) {
+        while (i * 2 + 1 < len) {
 
-            int j = k * 2 + 1;
-            // 最大的子节点
+            int j = i * 2 + 1;
+            // 找出最大的子节点，j是左节点 j+1是右节点
             if (j + 1 < len && array[j+1] > array[j]) {
                 j++;
             }
 
             // 不需要调整，直接退出
-            if (tmp >= array[j]) {
+            if (array[i] >= array[j]) {
                 break;
             }
 
-            swap(array, k, j);
+            swap(array, i, j);
 
-            // 子节点j的值和父节点k交换了，
+            // 子节点j的值和父节点i交换了，
             // 现在要看变化后的子节点j作为父节点，是否满足要求（子节点可能也是父节点）
-            k = j;
+            i = j;
         }
 
     }
