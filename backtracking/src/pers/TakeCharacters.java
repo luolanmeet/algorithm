f534321c740553efef003e01a8003fac2c3c0220
luolanmeet <mainem@163.com>
2024-09-27 20:51:15 +0800

    add lcgs

---
+++ backtracking/src/pers/TakeCharacters.java
@@ -0,0 +1,58 @@
+package pers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 每种字符至少取 K 个
+ * https://leetcode.cn/problems/take-k-of-each-character-from-left-and-right/submissions/568611350/
+ * @auther ken.ck
+ * @date 2024/9/27 19:59
+ */
+public class TakeCharacters {
+
+    Set<String> set = new HashSet<>();
+    int res = 0;
+    public int takeCharacters(String s, int k) {
+        int[] count = new int[3];
+        res = Integer.MAX_VALUE;
+        set = new HashSet<>();
+        backTrack(s, 0, s.length() - 1, count, 0, k);
+        return res == Integer.MAX_VALUE ? -1 : res ;
+    }
+
+    public void backTrack(String s, int left, int right, int[] count, int time, int k) {
+
+        // 超时了。回溯本质上就是暴力，只是加上一些剪枝可以进行优化
+
+        if (count[0] >= k && count[1] >= k && count[2] >= k) {
+            res = Math.min(res, time);
+            return ;
+        }
+        if (left > right || time >= res) {
+            return ;
+        }
+        if (set.contains(left + " " + right)) {
+            return;
+        }
+        set.add(left + "" + right);
+
+        time++;
+
+        // 尝试取走左边
+        count[s.charAt(left) - 'a']++;
+        backTrack(s, left + 1, right, count, time, k);
+        count[s.charAt(left) - 'a']--; // 还原
+
+        // 尝试取走右边
+        count[s.charAt(right) - 'a']++;
+        backTrack(s, left, right - 1, count, time, k);
+        count[s.charAt(right) - 'a']--; // 注意这里也要还原
+    }
+
+    public static void main(String[] args) {
+        TakeCharacters obj = new TakeCharacters();
+        System.out.println(obj.takeCharacters("aabaaaacaabc", 2));
+    }
+
+}
---
+++ other/src/pers/doublePoint/slidingWindow/TakeCharacters.java
@@ -0,0 +1,43 @@
+package pers.doublePoint.slidingWindow;
+
+/**
+ * 每种字符至少取 K 个
+ * https://leetcode.cn/problems/take-k-of-each-character-from-left-and-right/description/
+ * @auther ken.ck
+ * @date 2024/9/27 20:41
+ */
+public class TakeCharacters {
+
+    public int takeCharacters(String s, int k) {
+        int[] cnt = new int[3];
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            cnt[s.charAt(i) - 'a']++;
+        }
+        if (cnt[0] < k || cnt[1] < k || cnt[2] < k) {
+            return -1;
+        }
+
+        // 转换成取最长子串
+        int l = 0;
+        int r = 0;
+        int ans = s.length();
+        for ( ; r < len; r++) {
+            cnt[s.charAt(r) - 'a']--;
+            while (l < r && (cnt[0] < k || cnt[1] < k || cnt[2] < k)) {
+                cnt[s.charAt(l) - 'a']++;
+                l++;
+            }
+            if (cnt[0] >= k && cnt[1] >= k && cnt[2] >= k) {
+                ans = Math.min(ans, len - (r - l + 1));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        TakeCharacters obj = new TakeCharacters();
+        System.out.println(obj.takeCharacters("aabaaaacaabc", 2));
+    }
+
+}
